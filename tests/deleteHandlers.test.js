b23c85b8d0c9f94f430fd587719b02e7d831d638
Practicum Student <practicum_student@gmail.com>
2023-03-14 19:37:10 +0300

    5

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -4,7 +4,7 @@ const config = require('../config');
 test('status should be 200', async () => {
     let actualStatus;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/788888`, {
 			method: 'DELETE',
 		});
         actualStatus = response.status;
@@ -14,3 +14,14 @@ test('status should be 200', async () => {
     expect(actualStatus).toBe(200);
 });
 
+test('status should be 200', async () => {
+    let actualStatus;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7/products`);
+        console.log(await response.json())
+        actualStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+    expect(actualStatus).toBe(200);
+});
